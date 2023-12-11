fe5c1835dafdbee5942d138aceb003e5899c94ed
Corbin Meier <corbinivon@gmail.com>
2023-12-10 19:06:10 -0800

    Removes today and now defaults

--- src/app/components/DiabeticEntryForm.jsx
+++ src/app/components/DiabeticEntryForm.jsx
@@ -7,20 +7,20 @@ export default function DiabeticEntryForm() {
   const now = moment().format('HH:mm')
   return (
     <form action={InsertData} className="m-4">
-      <div className="px-4">
-        <label htmlFor="value" className="block text-sm font-medium text-gray-400">Reading</label>
-        <div className="flex">
-          <input type="number" name="value" id="value" className="bg-pink-900 text-white rounded-md w-full p-5" placeholder="mg/dL Reading" required/>
-        </div>
-      </div>
       <div className="flex justify-between px-4 pt-2">
         <div>
           <label htmlFor="date" className="block text-sm font-medium text-gray-400">date</label>
-          <input type="date" name="date" id="date" className="bg-pink-900 text-white rounded-md w-full p-1" value={today} autofocus/>
+          <input type="date" name="date" id="date" className="bg-pink-900 text-white rounded-md w-full p-1"/>
         </div>
         <div>
           <label htmlFor="time" className="block text-sm font-medium text-gray-400">time</label>
-          <input type="time" name="time" id="time" className="bg-pink-900 text-white rounded-md w-full p-1" value={now}/>
+          <input type="time" name="time" id="time" className="bg-pink-900 text-white rounded-md w-full p-1" />
+        </div>
+      </div>
+      <div className="px-4">
+        <label htmlFor="value" className="block text-sm font-medium text-gray-400">Reading</label>
+        <div className="flex">
+          <input type="number" name="value" id="value" className="bg-pink-900 text-white rounded-md w-full p-5" placeholder="mg/dL Reading" required autofocus/>
         </div>
       </div>
       <div className="flex justify-center m-4">
